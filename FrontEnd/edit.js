e65120e493147316c4150b425e487965a56394df
Kevin Farcy <farcy.kevin@gmail.com>
2024-08-07 02:25:36 +0200

    Show, close dialog

--- FrontEnd/edit.js
+++ FrontEnd/edit.js
@@ -2,13 +2,36 @@ document.addEventListener("DOMContentLoaded", function() {
     const editBar = document.getElementById("editBar");
     const modifier = document.querySelector(".modifier");
     const token = localStorage.getItem("token");
+    const dialog = document.querySelector("dialog");
+    const closeIcon = document.querySelector(".fa-xmark");
 
     if (token) {
-        if (editBar) {
-            editBar.style.display = "flex";
+        editBar.style.display = "flex";
+        modifier.style.display = "flex";
+        modifier.addEventListener("click", function() {
+            dialog.style.display = "flex";
+            dialog.showModal();
+        });
+    };
+
+    closeIcon.addEventListener("click", function() {
+        dialog.style.display = "none";
+        dialog.close();
+    });
+    
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape" || event.key === 27) {
+                dialog.style.display = "none";
+                dialog.close();
         }
-        if (modifier) {
-            modifier.style.display = "flex";
+    });
+
+    dialog.addEventListener("click", function(event) {
+        if (event.target === dialog) {
+            dialog.style.display = "none";
+            dialog.close();
         }
-    }
+    });
 });
+
+
