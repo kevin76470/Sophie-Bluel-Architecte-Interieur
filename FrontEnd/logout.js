67c612b7e7399d6cc66ccc368b2ae99b85dbc0f7
Kevin Farcy <farcy.kevin@gmail.com>
2024-08-21 11:46:35 +0200

    Optimisation/Simplification du code

--- FrontEnd/logout.js
+++ FrontEnd/logout.js
@@ -1,19 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const token = localStorage.getItem("token");
-    const loginLink = document.getElementById("login-link");
-    const logoutLink = document.getElementById("logout-link");
+const userToken = localStorage.getItem("token");
+const loginLink = document.getElementById("login-link");
+const logoutLink = document.getElementById("logout-link");
 
-    if (token) {
-        loginLink.style.display = "none";
-        logoutLink.style.display = "block";
-    } else {
-        loginLink.style.display = "block";
-        logoutLink.style.display = "none";
-    }
+if (userToken) {
+    loginLink.style.display = "none";
+    logoutLink.style.display = "block";
+} else {
+    loginLink.style.display = "block";
+    logoutLink.style.display = "none";
+}
 
-    logoutLink.addEventListener("click", function(event) {
-        event.preventDefault();
-        localStorage.removeItem("token");
-        window.location.href = "./index.html";
-    });
+logoutLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    localStorage.removeItem("token");
+    window.location.href = "./index.html";
 });
